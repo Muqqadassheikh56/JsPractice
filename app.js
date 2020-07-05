54645d6b36375ee310f87926aedca0b0f1450298
Muqqadas Sheikh <Muqqadassheikh56@gmail.com>
2020-07-05 13:48:29 +0500

    button files

---
+++ app.js
@@ -0,0 +1,10 @@
+function btnColor(){
+    var button = document.body.style.background = "red";
+}
+
+function btnColor1(){
+    var button = document.body.style.background = "blue";
+}
+function btnColor2(){
+    var button = document.body.style.background = "orange";
+}
